7bc0d816c3aebbbf1b9b489f1325bb8705603912
Calvin Shin <calvin.shin@gmail.com>
2019-04-08 00:09:58 -0700

    Updated the monster items to dynamically generate and be reloaded. Need to add functionality for the quiz elements

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,6 +4,7 @@
 
 var game = {
     monsterList : ["green_dragon", "fire_demon", "ghost", "medusa", "minotaur", "rat", "wolf", "worm"],
+    // created dynamically, but is essentially derived from the monsterList to create a single object for each monster
     monsterObjects : [],
     backgroundArray : ["assets/images/background/fantasy-1920-x-1080_full - Copy (2).png",
         "assets/images/background/fantasy-1920-x-1080_full - Copy (3).png",
@@ -49,36 +50,88 @@ var game = {
     // Active variables for the game
     currentQuestion : 0,
     activeBackground : "",
-    activeMonster1 :  "",
-    activeMonster2 :  "",
+    activeQuestion : "",
+    activeMonster1 : "",
+    activeMonster2 : "",
     activeMonster3 : "",
     activeMonster4 : "",
 
     // functions
+    // Creating the array of objects for monsters. Has no question information, just creates the list of monsters to use in the game.
+    monsterSelection: function() {
+        for(i=0; i < game.monsterList.length; i++) {
+            var monster = [];
+        
+            // creating the name of the monster with it capitalized
+            monster.name = game.monsterList[i].charAt(0).toLocaleUpperCase() + game.monsterList[i].slice(1);
+            if(monster.name.indexOf("_") === -1) {
+                // nothing happens
+            }
+            else {
+                // Removing a single _ in the name and replacing it with a space
+                monster.name = monster.name.slice(0, monster.name.indexOf("_")) + " " + monster.name.slice(monster.name.indexOf("_") + 1);
+            }
+            // Create the images for idle
+            monster.idle = "assets/images/monsters/" + game.monsterList[i] + "_idle.gif"
+            // Create the images for attack
+            monster.attack = "assets/images/monsters/" + game.monsterList[i] + "_attack.gif"
+            // Create the images for death
+            monster.death = "assets/images/monsters/" + game.monsterList[i] + "_death.gif"
+
+            // Find the size of the images
+            monster.onload = function() {
+                monster.width = monster.idle.naturalWidth;
+                monster.height = monster.idle.naturalHeight;
+            }
+        
+            // Create the question
+        
+            // push the entire monster into the monsterObjects array
+            game.monsterObjects.push(monster);
+        }
+    },
+
+    // Generates the monsters for the page. Will probably need to add a section to attach the questions.
     monsterGenerate : function() {
         // generate a monster for the number of answers 
-        // ~~~~~~~~~~~~~~~~~~~~~~~ will need to figure out a better way to hold all the questions and stuff...
-        for(let i=0; i < 4; i++) {
-            // choose a random monster and set it to the variable monsterChosen
-            var monsterChosen = this.monsterObjects[Math.floor(Math.random() * this.monsterObjects.length)];
-            var monster = new Image();
-            monster.src = monsterChosen.idle;
-            // console.log("hey this ran " + i + "times");
-            // Attach text to the image
-            // https://www.w3schools.com/graphics/canvas_text.asp
-    
-    
-            // have to do an onload.... otherwise the drawimage... doesn't work?
-            // create a monster at a location, and then offset by i * some value later
-            monster.onload = function() {
-                game.gameArea.context.drawImage(monster, i * 250, 500);
-            };
+        var chosen = this.monsterObjects[0];
+        // var image = new Image();
+        // image.src = chosen.idle;
+        // image.onload = function() {
+
+        // for loop, to attach the question/answer to each chosen
+
+
+            game.activeMonster1 = new game.component(chosen.idle, 0, 480, 0, 0, chosen);
+            game.activeMonster2 = new game.component(chosen.idle, 250, 480, 0, 0, chosen);
+            game.activeMonster3 = new game.component(chosen.idle, 50, 550, 0, 0, chosen);
+            game.activeMonster4 = new game.component(chosen.idle, 300, 550, 0, 0, chosen);
+            
+        // }
+        // What's happening is that things aren't... working in order
+        console.log(game.activeMonster1)
+
+        // for(let i=1; i < 5; i++) {
+        //     // choose a random monster and set it to the variable monsterChosen
+        //     var monsterChosen = this.monsterObjects[0];
+        //     // var monsterChosen = this.monsterObjects[Math.floor(Math.random() * this.monsterObjects.length)];
+        //     var monster = new Image();
+        //     monster.src = monsterChosen.idle;
+        //     game.activeMonster1 = new game.component(monsterChosen.idle, (i-1) * 250, 500, , , monsterObjects[0])
+        //     // console.log("hey this ran " + i + "times");
+        //     // Attach text to the image
+        //     // https://www.w3schools.com/graphics/canvas_text.asp
+        //     // have to do an onload.... otherwise the drawimage... doesn't work?
+        //     // create a monster at a location, and then offset by i * some value later
+        //     monster.onload = function() {
+        //         game.gameArea.context.drawImage(monster, (i-1) * 250, 500);
+        //     };
     
             // console.log(monsterChosen);
             // console.log(game.monsterObjects[Math.floor(Math.random()) * 2])
             // document.getElementById("testdiv").append(monster);
             
-        }
+        // }
     },
     backgroundGenerate : function() {
         // Create a new image for simplicity sake, but is not actually needed. Could just input the src into the function component
@@ -99,8 +152,16 @@ var game = {
         this.y = y;
         this.object = object
         this.update = function() {
+            // Checks if the width > 0, then returns the regular image
+            if(this.width > 0) {
                 game.gameArea.context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            }
+            else{
+                game.gameArea.context.drawImage(this.image, this.x, this.y);
+            }
         };
+        // https://stackoverflow.com/questions/8429011/how-to-write-text-on-top-of-image-in-html5-canvas
+        // if(object.answers !== "") {}
         // for monster objects we need to keep more details for the answers. Pulling the entire object allows us to not have to add details but use the object within the object
             // otherwise, create new attributes from object.
         // positioning, also not yet required;
@@ -114,54 +175,57 @@ var game = {
         // game.gameArea.clear();
         // game.activeBackground.newPos();    
         game.activeBackground.update();
-        // console.log(game.activeBackground)
-        // game.activeMonster1.newPos();
-        // game.activeMonster1.onload();
-        // game.activeMonster1.update();
-
+        game.activeMonster1.update();
+        game.activeMonster2.update();
+        game.activeMonster3.update();
+        game.activeMonster4.update();
+        // Ensure this last variable is always on bottom so that the image is always drawn last
         testvariablee.update();
     },
+    shuffle : function(array) {
+        // console.log(array);
+        for(i=0; i<array.length; i++) {
+            var newPosition = Math.floor(Math.random() * array.length);
+            // console.log(newPosition);
+            var held = array[newPosition];
+            // console.log(held);
+            array[newPosition] = array[i];
+            array[i] = held;
+        }
+        // console.log(array);
+    },
+    start : function() {
+        game.gameArea.start();
+        game.roundStart();
+        console.log(game.activeMonster1)
+        console.log(game.activeBackground)
+    },
+    // Separated out because there needs to be an intro/opening page which will need to be drawn prior to a round starting
+    roundStart : function() {
+        game.monsterSelection();
+        game.shuffle(game.monsterObjects);
+        game.monsterGenerate();
+        game.backgroundGenerate();
+    }
 }
 
-for(i=0; i < game.monsterList.length; i++) {
-    var monster = [];
-
-    // creating the name of the monster with it capitalized
-    monster.name = game.monsterList[i].charAt(0).toLocaleUpperCase() + game.monsterList[i].slice(1);
-    if(monster.name.indexOf("_") === -1) {
-        // nothing happens
-    }
-    else {
-        monster.name = monster.name.slice(0, monster.name.indexOf("_")) + " " + monster.name.slice(monster.name.indexOf("_") + 1);
-    }
-    // Create the images for idle
-    monster.idle = "assets/images/monsters/" + game.monsterList[i] + "_idle.gif"
-    // Create the images for attack
-    monster.attack = "assets/images/monsters/" + game.monsterList[i] + "_attack.gif"
-    // Create the images for death
-    monster.death = "assets/images/monsters/" + game.monsterList[i] + "_death.gif"
-
-    // Create the question
-
-    // push the entire monster into the monsterObjects array
-    game.monsterObjects.push(monster);
-}
-
-
+// Test variable to make sure that all the steps work. Will keep this on the canvas until game is created.
 var testvariablee = "sm";
 testvariablee = new game.component("assets/images/monsters/minotaur_attack.gif", 20, 20, 60, 60, "image");
-console.log(testvariablee)
+// console.log(testvariablee)
 
 
 // console.log(game)
 // console.log(game.monsterObjects)
-game.gameArea.start();
-game.backgroundGenerate();
+// game.monsterSelection();
+// game.gameArea.start();
+// game.backgroundGenerate();
 
 // document.addEventListener()
-game.monsterGenerate();
+// game.monsterGenerate();
 // game.activeBackground.update();
 // console.log(game.activeBackground)
 
-console.log(testvariablee)
-console.log(game.activeBackground)
+// document.onload = function() {
+    game.start();
+// };
